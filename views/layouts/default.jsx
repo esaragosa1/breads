10b345c15425fde1e0a61a65db20ba5697346634
esaragosa1 <108439543+esaragosa1@users.noreply.github.com>
2022-11-14 15:27:15 -0800

    sending index and show data

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -4,17 +4,16 @@ function Default(html) {
   return (
     <html>
     <head>
-      <title>Default</title>
+      <title>{html.title || 'Default'}</title>
     </head>
     <body>
       <h1>HTML Rendered!</h1>
-      <div className="container">
-        {html.children}
-      </div>
+      {html.children}
     </body>
     </html>
   )
 }
+
 
 module.exports = Default
 
